d751217b621f614133bd58aaf23fc7ed73cc7c47
antoni-codes <emmanuel_general@hotmail.com>
2022-05-01 13:51:34 -0500

    feat(web): modificación impresión de nombres

---
+++ LaunchX-NodeJs-S3-1_Creacion_proyectos_JS/my_launchx_app/index.js
@@ -0,0 +1,11 @@
+
+const MyMissionCommander = require('./app/MissionCommander');
+
+const emmaMC = new MyMissionCommander('Emmanuel')
+console.log(emmaMC.name);
+
+const carloMC = new MyMissionCommander('Carlo')
+console.log(carloMC.name);
+
+const lauraMC = new MyMissionCommander('Laura')
+console.log(lauraMC.name);
