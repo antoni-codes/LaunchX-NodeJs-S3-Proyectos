d2fc0d70ba6654f1b976e237d9dd0146236f261c
antoni-codes <emmanuel_general@hotmail.com>
2022-05-01 14:29:28 -0500

    test(app): creación de archivo test & prueba básica añadidad

---
+++ LaunchX-NodeJs-S3-1_Creacion_proyectos_JS/my_launchx_app/test/missionCommander.test.js
@@ -0,0 +1,8 @@
+
+// Prueba básica de Jest
+describe("Esto es un suite de pruebas", () => {
+    test('Caso de Prueba 1', () => {
+        const result = 1 + 2
+        expect(result).toBe(10)
+    })
+})
