96522a7840fe1f46d1eb9f255b6b645ead413395
antoni-codes <emmanuel_general@hotmail.com>
2022-05-01 13:17:51 -0500

    Creación de Clase y Exportación de módulo

---
+++ LaunchX-NodeJs-S3-1_Creacion_proyectos_JS/my_launchx_app/app/MissionCommander.js
@@ -0,0 +1,8 @@
+class MissionCommander {
+    constructor(name){
+        this.name = name;
+    }
+}
+
+// Esta línea nos permite exportar nuestra clase *Utilización de Common JS para exportar mómdulos*
+module.exports = MissionCommander;
