23f344e0dbcf4b1b02ae2adbe46b53012afcebd5
antoni-codes <emmanuel_general@hotmail.com>
2022-05-01 22:30:44 -0500

    feat: creación de método getInfo() de la clase Spiderman

--- LaunchX-NodeJs-S3-2_Test_Driven_Development/spiderverse/app/Spiderman.js
+++ LaunchX-NodeJs-S3-2_Test_Driven_Development/spiderverse/app/Spiderman.js
@@ -1,12 +1,18 @@
 class Spiderman {
-            constructor(name, age, actor, numMovies, producer) {
-                this.name = name
-                this.age = age
-                this.actor = actor
-                this.numMovies = numMovies
-                this.producer = producer
-            }
-        }
+    constructor(name, age, actor, numMovies, producer) {
+        this.name = name
+        this.age = age
+        this.actor = actor
+        this.numMovies = numMovies
+        this.studio = studio
+    }
+
+    getInfo() {
+        // "return" para regresar un string con interpolación donde puedas agregar las propiedades actor y studio 
+        return console.log(`Hey, I'm ${this.name} from ${this.studio}`)
+    }
+
+}
 
 
 
