2abad7e0bcf2c862d19e1343b785673a55ba2b58
antoni-codes <emmanuel_general@hotmail.com>
2022-05-03 13:31:59 -0500

    test: agregando código testing User class

--- LaunchX-NodeJs-S3-3_Twitter/twitter/test/models/User.test.js
+++ LaunchX-NodeJs-S3-3_Twitter/twitter/test/models/User.test.js
@@ -1,1 +1,20 @@
+const User = require('./../../app/models/User')
 
+
+describe("Unit Test fors for User Class", () => {
+    test("Create an User Object", () => {
+        // Aquí se invoca el código que seusarna en la App
+        const myUser = new User(1, 'antonicodes', 'Emmanuel', "Hi, i'm a Web Developer", 'dateCreated', 'lastUpdated')
+
+        // Aquí se validan los resultados de el código anterior
+        /*
+            Esta estructura es una comparación que va a igualar el valor de la izquierda con el valor 
+            de la derecha (valor esperado)
+        */
+        expect(myUser.id).toBe(1)
+        expect(myUser.userName).toBe('antonicodes')
+        expect(myUser.bio).toBe("Hi, i'm a Web Developer")
+        expect(myUser.dateCreated).toBe('dateCreated')
+        expect(myUser.lastUpdated).toBe('lastUpdated')
+    })
+})
