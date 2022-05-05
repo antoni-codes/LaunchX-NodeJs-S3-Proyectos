e6485ade40bc88f90b9071956f2c1a5ee0d718e1
antoni-codes <emmanuel_general@hotmail.com>
2022-05-05 00:37:49 -0500

    feat: agregando clase y pasando paramétros al constructor, y exportando la clase con commonJs

--- LaunchX-NodeJs-S3-3_Twitter/twitter/app/services/UserService.js
+++ LaunchX-NodeJs-S3-3_Twitter/twitter/app/services/UserService.js
@@ -1,1 +1,12 @@
 
+class UserService {
+    constructor(id, userName, name) {
+        this.id = id
+        this.userName = userName
+        this.name = name
+    }
+
+
+}
+
+module.exports = UserService
