990af123bbbee0960b7c1d174573eeaddbdc0f8c
antoni-codes <emmanuel_general@hotmail.com>
2022-05-05 00:26:38 -0500

    test: agregando código testing para la clase UserService

--- LaunchX-NodeJs-S3-3_Twitter/twitter/test/services/UserService.test.js
+++ LaunchX-NodeJs-S3-3_Twitter/twitter/test/services/UserService.test.js
@@ -1,1 +1,11 @@
+const UserService = require('./../../app/services/UserService')
 
+
+describe("Unit Test for UserService Class", () => {
+    test("1. Create a new user sing the myUserService", () => {
+        const myUserService = new UserService(1, 'antonicodes', 'Emmanuel Hernández')
+        expect(myUserService.id).toBe(1)
+        expect(myUserService.userName).toBe('antonicodes')
+        expect(myUserService.name).toBe('Emmanuel Hernández')
+    })
+})
