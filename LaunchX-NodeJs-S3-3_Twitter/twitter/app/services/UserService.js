767f8a0db2856354cb471961ebe173db919b183e
antoni-codes <emmanuel_general@hotmail.com>
2022-05-05 00:43:57 -0500

    fix: eliminando constructor

--- LaunchX-NodeJs-S3-3_Twitter/twitter/app/services/UserService.js
+++ LaunchX-NodeJs-S3-3_Twitter/twitter/app/services/UserService.js
@@ -1,10 +1,5 @@
 
 class UserService {
-    constructor(id, userName, name) {
-        this.id = id
-        this.userName = userName
-        this.name = name
-    }
 
     static create(id, userName, name,) {
         return new User(id, userName, name, "Sin bio")
