a880de16a263e6c5a03d0bebf7b64964960e833d
antoni-codes <emmanuel_general@hotmail.com>
2022-05-08 16:31:16 -0500

    test: agregando requerimiento 1 de views  en test y app

--- LaunchX-NodeJs-S3-3_Twitter/twitter/test/views/UserView.test.js
+++ LaunchX-NodeJs-S3-3_Twitter/twitter/test/views/UserView.test.js
@@ -1,1 +1,13 @@
 
+const UserView = require('./../../app/views/UserView')
+
+describe("Test for UserView", () => {
+
+    test("1. Return an error object try to create a new user with a null payload", () => {
+
+        const payload = null
+        const result = UserView.createUser(payload)
+        // https://jestjs.io/docs/using-matchers#strings
+        expect(result.error).toMatch(/payload no existe/)
+    })
+})
