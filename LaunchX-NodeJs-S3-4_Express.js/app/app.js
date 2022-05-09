0f08ef0ede6d3215bdbcad72ec050359121862c6
antoni-codes <emmanuel_general@hotmail.com>
2022-05-08 21:55:13 -0500

    feat: agregando código para correr servidor de Nodejs con Express.js

--- LaunchX-NodeJs-S3-4_Express.js/app/app.js
+++ LaunchX-NodeJs-S3-4_Express.js/app/app.js
@@ -1,1 +1,18 @@
 
+// Usando Objeto Express
+const express = require('express');
+// App de Express
+const app = express();
+// Puesto en que vamos a ver nuestras app en el navegador: localhost: 3000
+const port = 3000
+
+// Path inicial, responderá a la url localhost: 3000
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+// Con esto inicializamos esta App
+app.listen(port, () => {
+    console.log('Example app listening on port ${port}');
+    
+})
